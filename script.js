d51b42a856dc201e6bdf9209575efed57334440c
Paul Halliday <paulhalliday1979@gmail.com>
2015-02-03 10:26:23 -0330

    Add Angular Rocks button

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
   "use strict";
 
   var message = "Hello there!"
+  var message2 = "AngularJS Rocks!";
 
   document.getElementById('helloButton')
     .addEventListener('click', function() {
       alert(message);
     });
 
+  document.getElementById('rocksButton')
+    .addEventListener('click', function() {
+      alert(message2);
+    });
 }());
